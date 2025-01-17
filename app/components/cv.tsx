fb7312febb30af98d746b7defef02838a1ea0f12
Lucie monteiro <lucie.mtro@gmail.com>
2025-01-17 22:08:43 +0100

    test download cv

--- app/components/cv.tsx
+++ app/components/cv.tsx
@@ -1,7 +1,16 @@
 export default function Cv() {
   return (
-    <section id="cv" className="cv h-screen w-screen">
-      <h2 className="font-title text-center">Mon Curriculum Vitae</h2>
+    <section id="cv" className="cv h-screen w-screen flex flex-col p-4">
+      {/* Titre */}
+      <h2 className="font-title text-2xl text-center">Mon Curriculum Vitae</h2>
+
+      <a
+        href="./cvluciermonteiro.pdf"
+        download="cvluciemonteiro.pdf"
+        className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+      >
+        Télécharger en PDF
+      </a>
     </section>
   );
 }
