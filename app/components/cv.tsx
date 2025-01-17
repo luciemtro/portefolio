f5e601d22d80804f086ee67f9baa934e69975f42
Lucie monteiro <lucie.mtro@gmail.com>
2025-01-17 22:19:28 +0100

    add good link cv

--- app/components/cv.tsx
+++ app/components/cv.tsx
@@ -5,7 +5,7 @@ export default function Cv() {
       <h2 className="font-title text-2xl text-center">Mon Curriculum Vitae</h2>
 
       <a
-        href="./cvluciemonteiro.pdf"
+        href="https://github.com/luciemtro/portefolio/raw/main/app/components/cvluciemonteiro.pdf"
         download="cvluciemonteiro.pdf"
         className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
       >
