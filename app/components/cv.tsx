e0e883cbe87db757e871fb65210ac0a648c30e3a
Lucie monteiro <lucie.mtro@gmail.com>
2025-02-11 21:40:53 +0100

    Refactor Cv component for improved layout and spacing

--- app/components/cv.tsx
+++ app/components/cv.tsx
@@ -1,11 +1,8 @@
 export default function Cv() {
   return (
-    <section
-      id="cv"
-      className="w-screen flex flex-col items-center h-screen p-5"
-    >
-      <h2 className="font-title text-2xl text-center">Mon Curriculum Vitae</h2>
-      <div className="flex items-center justify-center py-28">
+    <section id="cv" className="w-screen flex flex-col items-center h-auto">
+      <h2 className="font-title text-2xl text-center ">Mon Curriculum Vitae</h2>
+      <div className="flex items-center justify-center pt-52 pb-28">
         <a
           href="https://github.com/luciemtro/portefolio/raw/main/app/components/cvluciemonteiro.pdf"
           download="cvluciemonteiro.pdf"
